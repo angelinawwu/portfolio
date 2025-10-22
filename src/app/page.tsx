c6997e10b43ca4c1fbbc85659d9a5e6d8e4e7caf
angelinawwu <angelinawu05@gmail.com>
2025-10-21 23:05:38 -0700

    comet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,30 @@ import Navigation from '@/components/Navigation';
 import Footer from '@/components/Footer';
 import ProjectCard from '@/components/ProjectCard';
 import { caseStudies, playgroundProjects } from '@/data/projects';
+import LiquidEther from '@/components/LiquidEther';
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-black">
+      <div className="fixed top-0 left-0 w-full h-full">
+        <LiquidEther
+          colors={[ '#A754DE', '#7694FF', '#97FFFF', '#C4E263', '#E29D63', '#F98BC7']}
+          mouseForce={20}
+          cursorSize={70}
+          isViscous={false}
+          viscous={30}
+          iterationsViscous={32}
+          iterationsPoisson={32}
+          resolution={0.5}
+          isBounce={false}
+          autoDemo={true}
+          autoSpeed={0.5}
+          autoIntensity={2.2}
+          takeoverDuration={0.25}
+          autoResumeDelay={3000}
+          autoRampDuration={0.6}
+        />
+      </div>
       <Navigation />
       
       <main className="pt-32">
